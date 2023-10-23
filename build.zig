93a67f9ffeecc2822db8babbe2b3e0ed51dfea8b
gryzus <gryzus24@gmail.com>
2023-10-23 12:36:02 +0200

    update build.zig

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const strip = b.option(bool, "strip", "Strip debug symbols");
 
     const exe = b.addExecutable(.{
         .name = "ziew",
@@ -10,23 +11,20 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
         .linkage = .static,
+        .link_libc = true,
         .single_threaded = true,
     });
-
-    if (optimize == .ReleaseFast) {
-        exe.strip = true;
+    exe.strip = strip orelse false;
+    if (optimize == .ReleaseFast)
         exe.omit_frame_pointer = true;
-    }
 
-    exe.linkLibC();
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
+    if (b.args) |args|
         run_cmd.addArgs(args);
-    }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
