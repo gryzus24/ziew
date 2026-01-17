eea2c457a4e1385e855133115d2dc00bddc53280
gryzus <gryzus24@gmail.com>
2026-01-17 02:52:23 +0100

    build: rewrite build.sh script
    * build.sh is meant to be a helper script that doesn't pollute user's
      home directory with unnecessary cache files and provides an overview
      of build options at a glance - implement the latter and simplify the
      script.
    * require bash - somewhat ironic given the "simplification" :-)

--- build.sh
+++ build.sh
@@ -1,42 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
 usage() {
-    printf '%s: <debug|small|fast> <install|test> [args...]\n' "$0"
+    printf '%s: [debug|small|fast|strip|native|omit-fp|no-oom-check|trace|
+             release|test...] \n' "$0"
 }
 
-OPT=
-case "$1" in
-    debug)
-        ;;
-    small)
-        OPT=-Doptimize=ReleaseSmall
-        ;;
-    fast)
-        OPT=-Doptimize=ReleaseFast
-        ;;
-    *)
-        usage
-        exit 1
-esac
-shift
-
 CACHE_DIR=/tmp/zig-ziew
-case "$1" in
-    install)
-        shift
-        zig build \
-            --summary none \
-            --cache-dir "$CACHE_DIR" \
-            -p . $OPT "$@"
-        ;;
-    test)
-        shift
-        set -x
-        zig build --cache-dir "$CACHE_DIR" $OPT "$@" test
-        set +x
-        ;;
-    *)
-        usage
-        exit 1
-        ;;
-esac
+
+TEST=
+declare -a FLAGS
+for arg in "$@"; do
+    case "$arg" in
+        debug)        FLAGS+=(-Doptimize=Debug) ;;
+        small)        FLAGS+=(-Doptimize=ReleaseSmall) ;;
+        fast)         FLAGS+=(-Doptimize=ReleaseFast) ;;
+        strip)        FLAGS+=(-Dstrip) ;;
+        native)       FLAGS+=(-Dmarch=native) ;;
+        omit-fp)      FLAGS+=(-Domit-frame-pointer) ;;
+        no-oom-check) FLAGS+=(-Dmem-no-oom-check) ;;
+        trace)        FLAGS+=(-Dmem-trace-allocations) ;;
+        release)      FLAGS=(-Doptimize=ReleaseSmall -Dstrip) ;;
+        test)         TEST=1 ;;
+        *)
+            printf '%s: unknown option: %s\n' "$0" "'$arg'"
+            usage
+            exit 1
+    esac
+done
+
+if [[ -n "$TEST" ]]; then
+    set -x
+    zig build --cache-dir "$CACHE_DIR" "${FLAGS[@]}" test
+    set +x
+else
+    zig build --summary none --cache-dir "$CACHE_DIR" -p . "${FLAGS[@]}"
+fi
