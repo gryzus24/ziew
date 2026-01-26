c0d2bd64fe9ab4202ee12def0df125b2b8f5c452
gryzus <gryzus24@gmail.com>
2026-01-26 22:04:10 +0100

    version 0.0.13

--- src/main.zig
+++ src/main.zig
@@ -64,7 +64,7 @@ const Args = struct {
                 _ = uio.sys_write(2, "required argument: c <path>\n");
             },
             .h => _ = uio.sys_write(2, "usage: ziew [c <config file>] [h] [v]\n"),
-            .v => _ = uio.sys_write(2, "ziew 0.0.12\n"),
+            .v => _ = uio.sys_write(2, "ziew 0.0.13\n"),
         }
         linux.exit(0);
     }
