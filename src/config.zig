3ef2da67d1e516952c7e6252be5ee48b7711ee52
gryzus <gryzus24@gmail.com>
2026-06-21 14:16:04 +0200

    config: update default config file
    * use some more recently added features and make it more similar to my
      config file.

--- src/config.zig
+++ src/config.zig
@@ -501,21 +501,33 @@ fn parseLine(tmp: *umem.Region, line: []const u8) !ParseLineResult {
 
 pub fn defaultConfig(reg: *umem.Region) []typ.Widget {
     const config =
-        \\NET 20 arg enp5s0 format "{arg} {inet}"
+        \\NET 20 arg eth0 format "{arg} {inet}"
         \\FG state 0:a44 1:4a4
-        \\NET 20 arg enp5s0 format "RxTx {rx_bytes:>}/{tx_bytes:>} {rx_pkts@dq:>2}/{tx_pkts@dq:<2}"
-        \\FG 686
-        \\CPU 20 format "{blkbars} {all:>3} {sys:>3}"
+        \\
+        \\NET 20 arg eth0 format "{rx_bytes@a:<} {rx_pkts@ad:2} ↓↑ {tx_pkts@ad:<2} {tx_bytes@a}"
+        \\FG 7a7
+        \\
+        \\DISK 200 arg / format "{arg} {available:^}{ino_used@dq}"
+        \\FG %used 80:aa0 90:a20
+        \\
+        \\CPU 20 format "FIC {forks@adq:2} {intr@adq:3} {ctxt@adq:3}"
+        \\FG forks 100:aa0
+        \\
+        \\CPU 20 format "{brlbars} {%all@a:3} {%sys@a:2} {blkgraph:3}"
         \\FG %all 0:999 48: 60:ff0 66:fc0 72:f90 78:f60 84:f30 90:f00
-        \\MEM 20 format "MEM {%used:>} {free:>} [{cached:>}]"
+        \\
+        \\MEM 20 format "MEM {%used@a} {free@a} [{cached@a}]"
         \\FG %used 60:ff0 66:fc0 72:f90 78:f60 84:f30 90:f00
-        \\MEM 20 format "{dirty@q:>.0}:{writeback@q:>.0}"
+        \\
+        \\MEM 20 format "{dirty@aq:.0}:{writeback@aq:.0}"
         \\FG 999
+        \\
         \\BAT 300 arg BAT0 format "BAT {%fulldesign:.2} {state}"
         \\FG state 1:4a4 2:4a4
         \\BG %fulldesign 0:a00 15:220 25:
-        \\TIME 20 arg "%A %d.%m ~ %H:%M:%S" format "{time}"
-        \\FG bb9
+        \\
+        \\TIME 20 arg "%A %d.%m - %H:%M:%S" format "{time}.{3}"
+        \\FG aa8
     ;
     var buffer: uio.Buffer = .fixed(config);
     var scratch: [128]u8 align(16) = undefined;
